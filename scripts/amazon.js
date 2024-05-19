b0e11ce467ed0ba401d6904ea09bed2f31acac61
hsrokon <hsrokonofficial@gmail.com>
2024-05-19 22:59:19 +0600

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,9 @@
 // Arrays and objects combination are used to create data structure 
-const products = [{
-  image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating : {
-    stars: 4.5,
-    count : 87
-  },
-  priceCents : 1090
-},
-{
-  image : 'images/products/intermediate-composite-basketball.jpg',
-  name : 'Intermediate Size Basketball',
-  rating : {
-    stars : 4,
-    count: 127
-  },
-  priceCents : 2095
-},
-{
-  image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg', 
-  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating : {
-    stars : 4.5,
-    counts : 56
-  },
-  priceCents : 799
-}];
+/* redirecting to products.js
+
+-we save the data for website in a data structure like products.js
+-then we use java script to generate the html using the data ( like from products.js)
+*/
 
 //combining all the html together
 let productsHTML ='';
