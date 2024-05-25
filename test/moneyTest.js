44fb07c9113511a3aba341ef707775a36b9d1c4a
hsrokon <hsrokonofficial@gmail.com>
2024-05-26 03:30:51 +0600

    Create automated tests for formatCurrency

---
+++ test/moneyTest.js
@@ -0,0 +1,35 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+console.log('text suite: formatCurrency')
+
+console.log('converts cents into dollers');
+//Basic test cases
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with zero');
+//Edge test cases
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cents');
+//Edge test cases
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('checks rounding of .4');
+//Edge test cases
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
